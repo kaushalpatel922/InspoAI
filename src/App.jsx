0dbd4fc192341e2c2d673f4d2712da30d663f480
Kaushal Patel <kaushalpatel922@gmail.com>
2025-04-07 16:38:10 -0400

    removing default react and vite, adding html selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [type, setType] = useState("blog");
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>InspoAI</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is fs {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <label>What type of inspiration?</label>
+        <select value={type} onChange={(e) => setType(e.target.value)}>
+          <option value="blog">Blog</option>
+          <option value="social">Social</option>
+          <option value="code">Code snippets</option>
+          <option value="email">Marketing Email</option>
+        </select>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
