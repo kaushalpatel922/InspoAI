4f4a61f979ba8706e966a69781f248e4e8b6c179
Kaushal Patel <kaushalpatel922@gmail.com>
2025-04-07 16:55:26 -0400

    adding handleGenerate and fetchResponse

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import "./index.css";
+import { fetchAIResponse } from "./utils/openai";
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [output, setOutput] = useState("");
   const [type, setType] = useState("blog");
 
+  const handleGenerate = async () => {
+    setOutput("Generating...");
+    const aiResponse = await fetchAIResponse(prompt, type);
+    setOutput(aiResponse);
+  };
   return (
     <>
       <div>
@@ -18,6 +26,10 @@ function App() {
           <option value="code">Code snippets</option>
           <option value="email">Marketing Email</option>
         </select>
+      </div>
+      <div>
+        <button onClick={handleGenerate}>Generate</button>
+        {output}
       </div>
     </>
   );
